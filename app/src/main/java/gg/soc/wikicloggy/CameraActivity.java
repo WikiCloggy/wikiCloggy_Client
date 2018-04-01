523e0fe6c30aabc846e9231052f67c3ee650059d
userp <jhikm1003@gmail.com>
2018-04-01 16:36:47 +0900

    add navigation bar

--- app/src/main/java/gg/soc/wikicloggy/CameraActivity.java
+++ app/src/main/java/gg/soc/wikicloggy/CameraActivity.java
@@ -1,13 +1,46 @@
 package gg.soc.wikicloggy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends Activity {
 
+    ListView listView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //For fullscreen with actionbar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_camera);
+
+        setCustomActionbar();
+
+        final String[] items = {"A", "B"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        listView = (ListView)findViewById(R.id.drawer_menulist);
+        listView.setAdapter(adapter);
+
+    }
+    //actionbar customizing
+    private void setCustomActionbar() {
+        ActionBar actionBar = getActionBar();
+
+        //for custom actionbar, set customEnabled true
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.layout_actionbar, null);
+
+        actionBar.setCustomView(actionbar);
     }
 }
