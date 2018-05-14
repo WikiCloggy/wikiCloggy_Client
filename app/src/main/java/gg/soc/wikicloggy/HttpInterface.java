d9f86ddee5d8e78706848006f73ba254a2bc0bf2
userp <jhikm1003@gmail.com>
2018-05-14 21:14:48 +0900

    서버와의 통신 추가

--- app/src/main/java/gg/soc/wikicloggy/HttpInterface.java
+++ app/src/main/java/gg/soc/wikicloggy/HttpInterface.java
@@ -1,8 +1,48 @@
 package gg.soc.wikicloggy;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
 /**
  * Created by userp on 2018-05-14.
  */
 
-public class HttpInterface {
+public class HttpInterface extends AsyncTask<Void, Void, String>{
+
+    private final static String TAG = "HttpInterface";
+
+    private final static String url = "http://ec2-13-125-187-247.ap-northeast-2.compute.amazonaws.com:3000";
+    //특정 user_code의 정보 보기
+    private final static String get_user_url = "/api/user/detail/"; //+user_code
+    //해당 user_code의 profile 정보 수정
+    private final static String post_user_profile_url = "/api/user/profile/"; //+user_code
+    //user_code profile 사진 path 설정
+    private final static String post_user_profile_image_url = "/api/user/profile/files/"; //+user_code
+    //user create
+    private final static String post_user_create_url = "/api/user/";
+
+    RequestHttpURLConnection urlConnection = new RequestHttpURLConnection();
+
+    private long userCode;
+    private String userName;
+    private Bitmap userImage;
+
+    public HttpInterface (User user) {
+        this.userCode = user.getId();
+        this.userImage = user.getBitmapImg();
+        this.userName = user.getName();
+    }
+    public HttpInterface() {
+
+    }
+    @Override
+    protected String doInBackground(Void... voids) {
+        return null;
+    }
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+
+    }
 }
