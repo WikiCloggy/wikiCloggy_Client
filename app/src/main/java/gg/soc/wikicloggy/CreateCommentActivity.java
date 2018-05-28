02828a1d2a1e776397efd526f2d2e5cecdfbe3cb
userp <jhikm1003@gmail.com>
2018-05-28 17:51:48 +0900

    댓글 작성/게시글 작성 레이아웃 완료

--- app/src/main/java/gg/soc/wikicloggy/CreateCommentActivity.java
+++ app/src/main/java/gg/soc/wikicloggy/CreateCommentActivity.java
@@ -1,9 +1,10 @@
 package gg.soc.wikicloggy;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateCommentActivity extends AppCompatActivity {
+public class CreateCommentActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
